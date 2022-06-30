c2860b1f7a64464031ea3d9df0d0de65ce47ce28
pankesh18 <wadekar.pankesh@gmail.com>
2022-06-30 16:27:05 +0100

    side nav

--- LMS/LMS-Web/src/app/app.module.ts
+++ LMS/LMS-Web/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
-
+import { SidebarModule } from 'primeng/sidebar';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { HomeComponent } from './home/home.component';
+import { ButtonModule } from 'primeng/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -15,8 +17,11 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AuthModule,
     DashboardModule,
+    SidebarModule,
+    ButtonModule,
     AppRoutingModule
   ],
   providers: [],
